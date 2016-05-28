d36dc52af4b5cf33ae222621ecb9ea65ffa8ba38
Ryan <lifezqy@126.com>
2016-05-28 22:55:59 +0800

    u

---
+++ binary_search.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  BinarySearch
+ *  Description:  
+ * =====================================================================================
+ */
+    int
+BinarySearch ( int *array, int key, int low, int high )
+{
+    int mid;
+    while(low <= high)
+    {
+        mid = (low+high)/2;
+        if(key == array[mid])
+        {
+            return mid;
+        }
+        else if(key < array[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return 0;
+}		/* -----  end of function BinarySearch  ----- */
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+    int
+main (  )
+{
+    int n, i, key, position;
+    int *array;
+    printf("请输入有序数组的大小：");
+    scanf("%d", &n);
+    array = (int*)malloc(sizeof(int)*n);
+    printf("请按升序输入数据：\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    printf("请输入想要查找的数：\n");
+    scanf("%d", &key);
+    if(position = BinarySearch(array, key, 0, n-1))
+    {
+        printf("%d的位置为：%d\n", key, position);
+    }
+    else
+    {
+        printf("%d不存在.\n");
+    }
+    return 0;
+}		/* -----  end of function main  ----- */
